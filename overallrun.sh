3bb30f757188d01b84c7593bdff7a5f074ea2896
Saurav <musunurusaurav@gmail.com>
2019-10-09 23:03:32 +0530

    Added executable permissions

--- overallrun.sh
+++ overallrun.sh
@@ -1,6 +1,6 @@
 python generate.py $2 $3 $4 $5 > $1.graphs 
-sh compile.sh
-sh run1.sh $1
+./compile.sh
+./run1.sh $1
 ./minisat $1.satinput $1.satoutput > $1.satlog
-sh run2.sh $1
+./run2.sh $1
 python check.py $1.graphs $1.mapping
